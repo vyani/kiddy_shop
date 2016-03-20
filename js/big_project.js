af0cd5cd9645d0e55f7f9fd9917dc510912bc7b4
vm <vyanimanao@gmail.com>
2016-03-20 21:59:26 +0700

    Move "onload=setupblocks" from div product to body
    :))))))

--- js/big_project.js
+++ js/big_project.js
@@ -1,37 +1,43 @@
-var colCount = 3;
-var colWidth = 50;
-var margin = 20;
-var windowWidth = 190;
-var blocks = [20,95,75];
+/*var colCount = 0;
+var colWidth = 314;
+var margin = 10;
+var spaceLeft = 20;
+var windowWidth = 1200;
+var blocks = [];
+
+$(function(){
+	$(window).resize(setupBlocks);
+});
 
 function setupBlocks() {
-    windowWidth = $(window).width();
-    colWidth = $('.block').outerWidth();
-    colCount = Math.floor(windowWidth/(colWidth+margin));
-    for(var i=0; i < colCount; i++) {
-        blocks.push(margin);
-    }
-    alert(blocks);
+	windowWidth = $(window).width();
+	blocks = [];
+
+	// Calculate the margin so the blocks are evenly spaced within the window
+	colCount = Math.floor(windowWidth/(colWidth+margin*2));
+	spaceLeft = (windowWidth - ((colWidth*colCount)+(margin*(colCount-1)))) / 2;
+	console.log(spaceLeft);
+	
+	for(var i=0;i<colCount;i++){
+		blocks.push(margin);
+	}
+	positionBlocks();
 }
 
-
 function positionBlocks() {
-    $('.block').each(function(){
-        var min = Array.min(blocks);
-        var index = $.inArray(min, blocks);
-        var leftPos = margin+(index*(colWidth+margin));
-        $(this).css({
-            'left':leftPos+'px',
-            'top':min+'px'
-        });
-        blocks[index] = min+block.outerHeight()+margin;
-    });
+	$('.block').each(function(i){
+		var min = Array.min(blocks);
+		var index = $.inArray(min, blocks);
+		var leftPos = margin+(index*(colWidth+margin));
+		$(this).css({
+			'left':(leftPos+spaceLeft)+'px',
+			'top':min+'px'
+		});
+		blocks[index] = min+$(this).outerHeight()+margin;
+	});	
 }
 
 // Function to get the Min value in Array
-Array.min = function() {
+Array.min = function(array) {
     return Math.min.apply(Math, array);
-};
-
-
-$(window).resize(setupBlocks);
+};*/
